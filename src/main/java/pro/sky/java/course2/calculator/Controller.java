a12c196ee4f6a8072fea0cf55b3c49db796f04c9
vinmash <vinmash@mail.ru>
2023-03-22 01:41:28 +0500

    done

--- src/main/java/pro/sky/java/course2/calculator/Controller.java
+++ src/main/java/pro/sky/java/course2/calculator/Controller.java
@@ -2,17 +2,42 @@ package pro.sky.java.course2.calculator;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping
+@RequestMapping("/calculator")
 public class Controller {
     private final Service service;
-    public Controller (Service service){
+
+    public Controller(Service service) {
         this.service = service;
     }
-    @GetMapping(path = "/calculator")
+
+    @GetMapping()
     public String hello() {
         return service.hello();
+    }
+
+    @GetMapping("plus")
+    public String plus(@RequestParam int num1, @RequestParam int num2) {
+        return num1 + " + " + num2 + " = " + service.plus(num1, num2);
+    }
+
+    @GetMapping("minus")
+    public String minus(@RequestParam int num1, @RequestParam int num2) {
+        return num1 + " - " + num2 + " = " + service.minus(num1, num2);
+    }
+
+    @GetMapping("multiply")
+    public String multiply(@RequestParam int num1, @RequestParam int num2) {
+        return num1 + " * " + num2 + " = " + service.multiply(num1, num2);
+    }
+
+    @GetMapping("divide")
+    public String divide(@RequestParam int num1, @RequestParam int num2) {
+        if (num2 == 0) {
+            return "На ноль делить нельзя!";
+        } else return num1 + " / " + num2 + " = " + service.divide(num1, num2);
     }
 }
